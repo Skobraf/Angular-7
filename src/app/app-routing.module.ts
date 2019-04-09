669a2d6a8a6b727677facb3a7b326a445aac0e55
skobraf <skobraf@gmail.com>
2019-04-09 09:39:11 +0100

    added child routes to recipe

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipesStartComponent } from './recipes/recipes-start/recipes-start.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-
 const appRoutes: Routes = [
     {path: '', redirectTo: '/recipes', pathMatch: 'full' },
-    {path: 'recipes', component: RecipesComponent},
+    {path: 'recipes', component: RecipesComponent, children: [
+        {path: '', component: RecipesStartComponent },
+        {path: ':id', component: RecipeDetailComponent }
+    ]},
     {path: 'shopping-list', component: ShoppingListComponent}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-it
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { DropdownDirective } from './shared/dropdown.directive';
+import { RecipesStartComponent } from './recipes/recipes-start/recipes-start.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -23,7 +24,8 @@ import { DropdownDirective } from './shared/dropdown.directive';
     RecipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
-    DropdownDirective
+    DropdownDirective,
+    RecipesStartComponent
   ],
   imports: [
     BrowserModule,
