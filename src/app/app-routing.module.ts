57e7ad1eab9c3d2a1b798b45192455faf027ff94
skobraf <skobraf@gmail.com>
2019-04-09 12:28:59 +0100

    add edit routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,15 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 
 const appRoutes: Routes = [
     {path: '', redirectTo: '/recipes', pathMatch: 'full' },
     {path: 'recipes', component: RecipesComponent, children: [
         {path: '', component: RecipesStartComponent },
-        {path: ':id', component: RecipeDetailComponent }
+        {path: 'new', component: RecipeEditComponent},
+        {path: ':id', component: RecipeDetailComponent },
+        {path: ':id/edit', component: RecipeEditComponent},
     ]},
     {path: 'shopping-list', component: ShoppingListComponent}
 ];
